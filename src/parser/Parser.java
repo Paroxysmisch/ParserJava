5fafafe2139d6b91a541a3bdb1b2c34c6a022e64
Yash Shah <ys562@cam.ac.uk>
2022-03-25 22:31:18 +0000

    Partial work on generating FIRST

--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -7,6 +7,7 @@ import java.util.*;
 public class Parser {
     private final Grammar grammar;
     private final Set<List<String>> nullable; // Represents grammar symbols which are nullable
+    private final Map<List<String>, Set<String>> first; // Represents the FIRST function which maps from a sequence of grammar symbols to the first terminal
 
     public Grammar getGrammar() {
         return grammar;
@@ -16,10 +17,11 @@ public class Parser {
         return nullable;
     }
 
-    public Parser(Grammar grammar, Set<List<String>> nullable) throws NullGrammarException {
+    public Parser(Grammar grammar, Set<List<String>> nullable, Map<List<String>, Set<String>> first) throws NullGrammarException {
         if (grammar == null) throw new NullGrammarException();
         this.grammar = grammar;
         this.nullable = Objects.requireNonNullElseGet(nullable, HashSet::new);
+        this.first = Objects.requireNonNullElseGet(first, HashMap::new);
     }
 
     public void generateNullable() {
@@ -45,5 +47,29 @@ public class Parser {
             if (!nullable.contains(List.of(grammarSymbol))) return false;
         }
         return true;
+    }
+
+    public void generateFirst() {
+        int lastFirstSize = 0;
+        first.put(List.of(""), new HashSet<>()); // The FIRST of epsilon is the empty set
+        // The FIRST of each terminal is the terminal itself (represented as a singleton list)
+        for (String terminal : grammar.getTerminals()) {
+            HashSet<String> set = new HashSet<>() {{
+                add(terminal);
+            }};
+            first.put(List.of(terminal), set);
+        }
+        while (first.size() > lastFirstSize) {
+            lastFirstSize = first.size();
+            // Loop through each production
+            for (Map.Entry<String, Set<List<String>>> productionSet : grammar.getProductions().entrySet()) {
+                for (List<String> production : productionSet.getValue()) {
+                    if (isNullable(production)) {
+                        nullable.add(production); // RHS of production is nullable
+                        nullable.add(List.of(productionSet.getKey())); // LHS of production (the non-terminal) is nullable
+                    }
+                }
+            }
+        }
     }
 }
--- src/parser/Test.java
+++ src/parser/Test.java
@@ -14,7 +14,7 @@ public class Test {
                 .addProduction("R", List.of(""))
                 .addProduction("R", List.of("b", "R"));
 
-        Parser parser = new Parser(grammar, null);
+        Parser parser = new Parser(grammar, null, null);
         parser.generateNullable();
         System.out.println(parser.getNullable());
     }
